8276af5d5d877d67f0a7d5995c0c1635760cee7e
Andrei <mojitoboss@gmail.com>
2022-12-04 21:58:57 +0300

    add dto and new data

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,12 +1,17 @@
-import { Module } from "@nestjs/common";
-import { TrackController } from "./track.controller";
-import { TrackService } from "./track.service";
-
+import {Module} from "@nestjs/common";
+import {TrackController} from "./track.controller";
+import {TrackService} from "./track.service";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Track, TrackSchema} from "./schema/track.schema";
+import {Comment, CommentSchema} from "./schema/comment.schema";
 
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{name: Track.name, schema: TrackSchema}]),
+        MongooseModule.forFeature([{name: Comment.name, schema: CommentSchema}]),
+    ],
     controllers: [TrackController],
     providers: [TrackService]
-
 })
 export class TrackModule {}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from "@nestjs/common";
-
+import { InjectModel } from "@nestjs/mongoose";
+import { Track } from "./schema/track.schema";
+import { TrackDocument } from "./schema/track.schema";
+import { CommentDocument } from "./schema/comment.schema";
+import { Model } from "mongoose";
+import { CreateTrackDto } from "./dto/create-track.dto";
 
 @Injectable()
 export class TrackService {
 
-    async create() {
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>){}
 
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({...dto, listens: 0})
+        return track;
     }
 
     async getAll() {
